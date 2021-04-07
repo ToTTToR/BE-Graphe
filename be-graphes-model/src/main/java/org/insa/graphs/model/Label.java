20ae033ed20799587c7407c6c4c6a44efe234168
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2021-04-07 18:12:21 +0200

    Djikstra Construction

---
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -0,0 +1,42 @@
+package org.insa.graphs.model;
+
+public class Label implements Comparable<Label>{
+	public Node SommetCourant;
+	
+	private boolean Marked;
+	
+	private double Cout;
+	
+	private Arc père;
+	
+	public double getCost() {
+		return this.Cout;
+	}
+	
+	public Label(Node sommetCourant, Arc père,double cout,boolean marked) {
+		this.Cout = cout;
+		this.SommetCourant = sommetCourant;
+		this.père = père;
+		this.Marked = marked;
+	}
+	
+	public void setCost(double newCost) {
+		this.Cout = newCost;
+	}
+	
+	public void setFather(Arc newFather) {
+		this.père = newFather;
+	}
+	
+	public void setMarked() {
+		this.Marked = true;
+	}
+	
+	public boolean isMarked() {
+		return this.Marked;
+	}
+	
+	public int compareTo(Label other) {
+        return Double.compare(Cout, other.Cout);
+    }
+}
