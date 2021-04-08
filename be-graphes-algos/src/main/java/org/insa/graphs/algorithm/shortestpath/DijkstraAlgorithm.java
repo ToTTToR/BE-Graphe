c7299c7bb468fc57b690a4d606b638da199044bc
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2021-04-08 11:07:14 +0200

    Première étape algo Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,11 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +16,24 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        ArrayList<Label> labels = new ArrayList<Label>();
+        Graph graph = data.getGraph();
+        BinaryHeap<Label> heap = new BinaryHeap();
+      //Phase d'Initialisation (Tous les sommets marqué faux, cout infini et aucun père.
+        for(int i=0;i<graph.getNodes().size();i++) {
+        	labels.add(new Label(graph.getNodes().get(i),null,Double.POSITIVE_INFINITY,false));
+        }
+        Label labelOrigin = labels.get(data.getOrigin().getId());
+        labelOrigin.setCost(0.0);;
+        heap.insert(labelOrigin);
+        while() {
+        	Label minTas = heap.findMin();
+        	minTas.setMarked();
+        	for(Arc successeurs : minTas.SommetCourant.getSuccessors()){
+        		if(labels.get(successeurs.getDestination().getId()))
+        	}
+        }
         return solution;
     }
 
