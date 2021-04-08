4e6b7348e0086d8fdcb3052bddc7e198b78f24d1
ToTTToR <vikinor007@gmail.com>
2021-04-08 18:42:06 +0200

    Dijkstra fait

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,6 +1,5 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -22,7 +21,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         
         ArrayList<Label> labels = new ArrayList<Label>();
         Graph graph = data.getGraph();
-        BinaryHeap<Label> heap = new BinaryHeap();
+        BinaryHeap<Label> heap = new BinaryHeap<Label>();
       //Phase d'Initialisation (Tous les sommets marqué faux, cout infini et aucun père.
         for(int i=0;i<graph.getNodes().size();i++) {
         	labels.add(new Label(graph.getNodes().get(i),null,Double.POSITIVE_INFINITY,false));
@@ -34,17 +33,22 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	Label minTas = heap.deleteMin();
         	minTas.setMarked();
         	for(Arc successeur : minTas.SommetCourant.getSuccessors()){
+        		//Il faut voir si le chemin est valide! Sinon on skip.
+        		if (!data.isAllowed(successeur)) {
+                    continue;
+                }
+        //Il faut voir si nous avons pas déjà marqué le sommet, c'est à dire que l'on ait déjà traité ou non
         		if(!labels.get(successeur.getDestination().getId()).isMarked()) {
         			double w = data.getCost(successeur);
-        			int IdSucc = successeur.getDestination().getId();
-        			int IdCourant = successeur.getOrigin().getId();
-        			if(labels.get(IdSucc).getCost() > labels.get(IdCourant).getCost() + w) {
-        				labels.get(IdSucc).setCost(labels.get(IdCourant).getCost() + w);
-        				labels.get(IdSucc).setFather(successeur);
-        				try {
-        					heap.remove(labels.get(IdSucc));
-        				} catch (ElementNotFoundException e) {}
-        				heap.insert(labels.get(IdSucc));
+        			Label LabelSucc = labels.get(successeur.getDestination().getId());
+        			Label LabelCourant = labels.get(successeur.getOrigin().getId());
+        			if(LabelSucc.getCost() > LabelCourant.getCost() + w) {
+        				LabelSucc.setCost(LabelCourant.getCost() + w);
+        				LabelSucc.setFather(successeur);
+        				try { //On regarde si l'élément est déjà dans la pile avec remove car on doit l'update
+        					heap.remove(LabelSucc);
+        				} catch (ElementNotFoundException e) {} //Sinon, il suffit juste d'insérer l'élément.
+        				heap.insert(LabelSucc);
         			}
         		}
         	}
