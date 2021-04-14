66b37554a0cb05d1e460d48e3a482c2b2a2e37f8
Viktor Mouton <7734587-TotttttoR@users.noreply.gitlab.com>
2021-04-14 16:41:59 +0200

    update sur Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 
 import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
-import org.insa.graphs.algorithm.utils.ElementNotFoundException;
 import org.insa.graphs.model.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
@@ -33,6 +32,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	Label minTas = heap.deleteMin();
         	minTas.setMarked();
         	notifyNodeMarked(minTas.SommetCourant);
+        	if(minTas.SommetCourant == data.getDestination()) break; //On a trouvé la destination
+        	//System.out.println("Nombre de successeurs : "+ minTas.SommetCourant.getNumberOfSuccessors());
         	for(Arc successeur : minTas.SommetCourant.getSuccessors()){
         		//Il faut voir si le chemin est valide! Sinon on skip.
         		if (!data.isAllowed(successeur)) {
@@ -44,18 +45,18 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         			Label LabelSucc = labels.get(successeur.getDestination().getId());
         			Label LabelCourant = labels.get(successeur.getOrigin().getId());
         			if(LabelSucc.getCost() > LabelCourant.getCost() + w) {
+        				//System.out.println("Mis à jour du coût de "+ LabelSucc.getCost() + " et "+ LabelCourant.getCost()+w);
+        				if(LabelSucc.getFather()!=null) heap.remove(LabelSucc); 
+        				//On regarde si le sommet a un père => si il est dans la pile.
         				LabelSucc.setCost(LabelCourant.getCost() + w);
-        				LabelSucc.setFather(successeur);
-        				try { //On regarde si l'élément est déjà dans la pile avec remove car on doit l'update
-        					heap.remove(LabelSucc);
-        				} catch (ElementNotFoundException e) {} //Sinon, il suffit juste d'insérer l'élément.
+        				LabelSucc.setFather(successeur); 
         				heap.insert(LabelSucc);
         			}
         		}
         	}
         }
         
-        if (labels.get(data.getDestination().getId()) == null) {
+        if (labels.get(data.getDestination().getId()).getFather() == null) {
             solution = new ShortestPathSolution(data, Status.INFEASIBLE);
         }
         else {
@@ -75,7 +76,11 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
             Collections.reverse(arcs);
 
             // Create the final solution.
-            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+            Path chemin = new Path(graph, arcs);
+            if(!chemin.isValid()) System.out.println("Chemin non valide!");
+            //System.out.println("Longueur du chemin : "+chemin.getLength());
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, chemin);
+            //System.out.println("Longueur du chemin par l'algo : "+solution.getPath().getLength());
         }
         
         return solution;
